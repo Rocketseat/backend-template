7619582d0f6968e90e1d0dd8ca7a5daefca6fd2d
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2022-03-31 20:26:50 -0300

    feat: Add readiness and liveness checks

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import tracer from 'dd-trace';
 import { LoggerErrorInterceptor } from 'nestjs-pino';
+import { ConfigurationInput, createLightship, Lightship } from 'lightship';
 
 import { LoggerService } from '@infra/logger/logger.service';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const configuration: ConfigurationInput = {
+    detectKubernetes: process.env.NODE_ENV !== 'production' ? false : true,
+    port: 9000,
+  };
+
+  const lightship: Lightship = await createLightship(configuration);
   const app = await NestFactory.create(AppModule);
+
+  lightship.registerShutdownHandler(() => app.close());
 
   const LoggerServiceInstance = app.get(LoggerService);
 
@@ -17,6 +26,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.listen(3333).then(() => {
+    lightship.signalReady();
     LoggerServiceInstance.log('HTTP server running!');
   });
 }
