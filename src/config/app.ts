c8f703d25379f75296a58d1dcb0f0c9d62146e5a
Jakson Xavier <35400037+jaksonxavier@users.noreply.github.com>
2023-04-11 18:41:30 +0000

    fix: setup dotenv

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export const PORT = process.env.PORT ?? 3333;
 export const ENV = process.env.NODE_ENV ?? 'development';
 export const SERVICE = process.env.SERVICE ?? 'backend-template';
